caa091e474181215d5f506347d62931cb0edd056
LucaLin <78164141+LucaLin233@users.noreply.github.com>
2021-04-24 20:47:09 +0800

    Update Sub.js

--- Profile/Sub.js
+++ Profile/Sub.js
@@ -1,4 +1,3 @@
 hostname=sub.store
-simple_cert_hostname = sub.store
 #Sub-Store
 ^https?:\/\/sub\.store url script-analyze-echo-response https://raw.githubusercontent.com/Peng-YM/Sub-Store/master/backend/sub-store.min.js
