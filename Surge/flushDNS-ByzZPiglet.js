6917e7dfbc955c9cc1b75a75388e99e01aac185d
LucaLin <78164141+LucaLin233@users.noreply.github.com>
2021-09-26 20:10:43 +0800

    Create flushDNS-ByzZPiglet.js

---
+++ Surge/flushDNS-ByzZPiglet.js
@@ -0,0 +1,31 @@
+/*
+[Script]
+flushDNS = type=generic,timeout=10,script-path=https://raw.githubusercontent.com/zZPiglet/Task/master/asset/flushDNS.js
+// use "icon" and "color" in "argument":
+// flushDNS = type=generic,timeout=10,script-path=https://raw.githubusercontent.com/zZPiglet/Task/master/asset/flushDNS.js,argument=icon=arrow.clockwise&color=#3d3d5b
+[Panel]
+flushDNS = script-name=flushDNS,update-interval=-1
+*/
+
+!(async () => {
+    let dnsCache = ""
+    let delay = ((await httpAPI("/v1/test/dns_delay")).delay * 1000).toFixed(0);
+    let panel = {
+        title: "Flush DNS",
+        content: `delay: ${delay}ms${dnsCache ? `\nserver:\n${dnsCache}` : ""}`,
+    };
+    if (typeof $argument != "undefined") {
+        let arg = Object.fromEntries($argument.split("&").map((item) => item.split("=")));
+        panel.icon = arg.icon;
+        panel["icon-color"] = arg.color;
+    }
+    $done(panel);
+})();
+
+function httpAPI(path = "", method = "POST", body = null) {
+    return new Promise((resolve) => {
+        $httpAPI(method, path, body, (result) => {
+            resolve(result);
+        });
+    });
+}
