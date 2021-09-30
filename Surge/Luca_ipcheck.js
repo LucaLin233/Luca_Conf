0093ea52e031a9e7a4e29f22f14472a17adce1dd
LucaLin <78164141+LucaLin233@users.noreply.github.com>
2021-10-01 03:11:42 +0800

    Update Luca_ipcheck.js

--- Surge/Luca_ipcheck.js
+++ Surge/Luca_ipcheck.js
@@ -1,3 +1,15 @@
+/**
+* 感谢@congcong大佬提供的js，原文件地址：https://github.com/congcong0806/surge-list/blob/master/Script/ipcheck.js
+* 用法
+* [Panel]
+* #节点检测
+* ipcheck = script-name=ipcheck, title="节点信息", content="请刷新", style=info, update-interval=1
+* ...
+* [Script]
+* #节点检测
+* ipcheck = type=generic,timeout=5,script-path=https://raw.githubusercontent.com/congcong0806/surge-list/master/Script/ipcheck.js
+*/
+
 let url = "http://ip-api.com/json"
 
 $httpClient.get(url, function(error, response, data){
