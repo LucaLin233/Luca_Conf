8b6a1bb4665b79c1609908f60f18ec2d7d4fdbe3
LucaLin <78164141+LucaLin233@users.noreply.github.com>
2021-10-01 03:04:46 +0800

    Create Luca_ipcheck.js

---
+++ Surge/Luca_ipcheck.js
@@ -0,0 +1,25 @@
+let url = "http://ip-api.com/json"
+
+$httpClient.get(url, function(error, response, data){
+    let jsonData = JSON.parse(data)
+    let ip = jsonData.query
+    let country = jsonData.country
+    let emoji = getFlagEmoji(jsonData.countryCode)
+    let city = jsonData.city
+    let isp = jsonData.isp
+  body = {
+    title: "节点信息",
+    content: `IP: ${ip}\nISP: ${isp}\n位置: ${emoji}${country} - ${city}`,
+    icon: "network"
+  }
+  $done(body);
+});
+
+
+function getFlagEmoji(countryCode) {
+    const codePoints = countryCode
+      .toUpperCase()
+      .split('')
+      .map(char =>  127397 + char.charCodeAt());
+    return String.fromCodePoint(...codePoints);
+}
