b9bbb61f6cd22afe87bd3ce9d402bbbf2941fe01
LucaLin <78164141+LucaLin233@users.noreply.github.com>
2021-10-02 05:38:31 +0800

    Update ipcheck.js

--- Surge/ipcheck.js
+++ Surge/ipcheck.js
@@ -14,6 +14,7 @@ let url = "http://ip-api.com/json/?lang=zh-CN"
 
 $httpClient.get(url, function(error, response, data){
     let jsonData = JSON.parse(data)
+    let ip = jsonData.query
     let country = jsonData.country
     let emoji = getFlagEmoji(jsonData.countryCode)
     let city = jsonData.city
