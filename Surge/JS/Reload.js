e4aca3c87860dc363a8059f15c75ee2f9dbeff66
LucaLin <78164141+LucaLin233@users.noreply.github.com>
2022-01-08 05:21:09 +0800

    Create Reload.js

---
+++ Surge/JS/Reload.js
@@ -0,0 +1,18 @@
+/*
+Author：Pysta
+GitHub Link:https://github.com/mieqq/mieqq
+* [Panel]
+* 配置重载 = title=配置重载,content=配置重载,style=info,script-name=配置重载,update-interval=-1
+* [Script]
+* 配置重载=script-path=https://raw.githubusercontent.com/TributePaulWalker/Profiles/main/JavaScript/Surge/Profile-Reload.js,type=generic
+*/
+
+$httpAPI("POST", "/v1/profiles/reload", {}, data => {
+    $notification.post("配置重载","配置重载成功","")
+    $done({
+        title: "配置重载",
+        content: "配置重载成功",
+        icon: "terminal",
+        "icon-color": "#5AC8FA",
+     })
+    });
