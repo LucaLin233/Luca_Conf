c781ff81827e264b1d0e3aab6b1a0059fe116acd
LucaLin <78164141+LucaLin233@users.noreply.github.com>
2022-01-08 05:28:26 +0800

    Update Reload.js

--- Surge/JS/Reload.js
+++ Surge/JS/Reload.js
@@ -12,7 +12,7 @@ $httpAPI("POST", "/v1/profiles/reload", {}, data => {
     $done({
         title: "配置重载",
         content: "配置重载成功",
-        icon: "terminal",
-        "icon-color": "#5AC8FA",
+        icon: "sparkles.tv",
+        "icon-color": "#FFCC00",
      })
     });
