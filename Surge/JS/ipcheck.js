258c632c0eba70be67a8bdac7f90e81539c806b1
LucaLin <78164141+LucaLin233@users.noreply.github.com>
2022-05-09 14:04:00 +0800

    Update ipcheck.js

--- Surge/JS/ipcheck.js
+++ Surge/JS/ipcheck.js
@@ -20,8 +20,8 @@ $httpClient.get(url, function(error, response, data){
     let isp = jsonData.isp
     let ip = jsonData.query
   body = {
-    title: "节点相关信息",
-    content: `IP信息: ${ip}\n运营商: ${isp}\n所在地: ${emoji}${country} - ${city}`,
+    title: "节点详情",
+    content: `「IP信息」：${ip}\n「运营商」：${isp}\n「所在地」：${emoji}${country} - ${city}`,
     icon: "globe.asia.australia.fill"
   }
   $done(body);
