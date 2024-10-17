b01609557d9a8ea4a591ccae6e797ec05fc9dfa4
LucaLin <78164141+LucaLin233@users.noreply.github.com>
2024-10-18 02:58:44 +0800

    Update alma_setup.sh

--- Other/alma_setup.sh
+++ Other/alma_setup.sh
@@ -18,12 +18,43 @@ systemctl restart sshd
 # 运行NextTrace安装脚本
 bash -c "$(curl -Ls https://github.com/sjlleo/nexttrace/raw/main/nt_install.sh)"
 
-# 设置BBR+FQ
-echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-sysctl -p
+# 运行内核调优
+bash -c "$(curl -Ls https://raw.githubusercontent.com/LucaLin233/Luca_Conf/main/Other/kernel_optimization.sh)"
 
 # 安装dnsutils
 dnf install -y bind-utils
+
+# 安装tuned
+dnf install tuned -y
+systemctl enable --now tuned
+
+# 检测并开启swap
+if ! swapon --show | grep -q "swap"; then
+    swapsize=$(( $(grep MemTotal /proc/meminfo | awk '{print $2}') * 2 * 1024 ))
+    fallocate -l ${swapsize} /swapfile
+    chmod 600 /swapfile
+    mkswap /swapfile
+    swapon /swapfile
+    echo '/swapfile none swap sw 0 0' >> /etc/fstab
+fi
+
+# 安装Python
+curl https://mise.run | sh
+mise use -g python@3.10
+
+# 启用zram
+dnf install zram-generator -y
+echo -e "[zram0]\nzram-size = ram / 2\ncompression-algorithm = zstd" | tee /etc/systemd/zram-generator.conf
+
+# 启动dnsproxy并替换系统dns
+cd /root/dnsproxy
+docker compose pull
+docker compose up -d
+bash <(curl -L -s https://raw.githubusercontent.com/1-stream/1stream-public-utils/main/dns-change.sh) 127.0.0.1
+
+# 启动proxy
+cd /root/proxy
+docker compose pull
+docker compose up -d
 
 echo "所有任务完成！"
