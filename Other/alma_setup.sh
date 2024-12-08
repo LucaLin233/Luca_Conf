59d10f466b17d9ef76cc1dd2255e200c12b090da
LucaLin <78164141+LucaLin233@users.noreply.github.com>
2024-12-08 10:02:12 +0800

    Update alma_setup.sh

--- Other/alma_setup.sh
+++ Other/alma_setup.sh
@@ -53,7 +53,6 @@ cd /root/proxy && docker compose pull && docker compose up -d
 
 # 启动其他容器
 cd /root/plmxs && docker compose pull && docker compose up -d
-cd /root/beszel && docker compose pull && docker compose up -d
 
 # 执行内核调优
 bash -c "$(curl -Ls https://raw.githubusercontent.com/LucaLin233/Luca_Conf/main/Other/kernel_optimization.sh)"
