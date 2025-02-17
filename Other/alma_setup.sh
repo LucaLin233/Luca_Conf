4ec80b30d41c629c6873107472e5fe69dd456658
LucaLin <78164141+LucaLin233@users.noreply.github.com>
2025-02-18 00:09:31 +0800

    Update alma_setup.sh

--- Other/alma_setup.sh
+++ Other/alma_setup.sh
@@ -81,12 +81,7 @@ check_error "添加新的swappiness设置"
 sysctl -w vm.swappiness=10
 check_error "应用swappiness设置"
 
-# 2. 执行内核调优
-echo "执行内核调优..."
-bash -c "$(curl -Ls https://raw.githubusercontent.com/LucaLin233/Luca_Conf/main/Other/kernel_optimization.sh)"
-check_error "执行内核调优"
-
-# 3. 更新软件包并安装必需的软件
+# 2. 更新软件包并安装必需的软件
 echo "更新系统并安装必要软件..."
 dnf update -y
 check_error "系统更新"
@@ -97,7 +92,7 @@ check_error "添加Docker仓库"
 dnf install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin bind-utils tuned zram-generator wget util-linux-user
 check_error "安装必要软件"
 
-# 4. 启用并立即启动 Docker 和 Tuned 服务
+# 3. 启用并立即启动 Docker 和 Tuned 服务
 echo "启用系统服务..."
 systemctl enable --now docker
 check_error "启用Docker服务"
@@ -105,12 +100,12 @@ check_error "启用Docker服务"
 systemctl enable --now tuned
 check_error "启用Tuned服务"
 
-# 5. 修改时区为上海
+# 4. 修改时区为上海
 echo "设置系统时区..."
 timedatectl set-timezone Asia/Shanghai
 check_error "设置时区"
 
-# 6. 修改SSH端口
+# 5. 修改SSH端口
 echo "配置SSH..."
 sed -i 's/#Port 22/Port 9399/' /etc/ssh/sshd_config
 check_error "修改SSH端口"
@@ -118,17 +113,17 @@ check_error "修改SSH端口"
 systemctl restart sshd
 check_error "重启SSH服务"
 
-# 7. 运行NextTrace安装脚本
+# 6. 运行NextTrace安装脚本
 echo "安装NextTrace..."
 bash -c "$(curl -Ls https://github.com/sjlleo/nexttrace/raw/main/nt_install.sh)"
 check_error "安装NextTrace"
 
-# 8. 启用zram
+# 7. 启用zram
 echo "配置ZRAM..."
 echo -e "[zram0]\nzram-size = ram / 2\ncompression-algorithm = zstd" | tee /etc/systemd/zram-generator.conf
 check_error "配置ZRAM"
 
-# 9. 启动容器
+# 8. 启动容器
 echo "启动容器..."
 cd /root && docker compose up -d
 check_error "启动root目录容器"
@@ -139,12 +134,12 @@ check_error "启动proxy容器"
 cd /root/vmagent && docker compose pull && docker compose up -d
 check_error "启动vmagent容器"
 
-# 10. 添加定时任务
+# 9. 添加定时任务
 echo "配置定时任务..."
 (crontab -l 2>/dev/null; echo "5 0 * * * dnf update -y") | crontab -
 check_error "添加定时任务"
 
-# 11. 安装fish
+# 10. 安装fish
 echo "安装fish shell..."
 cd /etc/yum.repos.d/
 wget https://download.opensuse.org/repositories/shells:fish:release:3/CentOS-9_Stream/shells:fish:release:3.repo
