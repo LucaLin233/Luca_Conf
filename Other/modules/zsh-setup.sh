323e0d4e30a08e7aebd800d5c8bded8e0acdd0b2
LucaLin <78164141+LucaLin233@users.noreply.github.com>
2025-07-26 01:15:03 +0800

    Update zsh-setup.sh

--- Other/modules/zsh-setup.sh
+++ Other/modules/zsh-setup.sh
@@ -120,7 +120,7 @@ CURRENT_SHELL=$(getent passwd root | cut -d: -f7)
 ZSH_PATH=$(which zsh)
 
 if [ "$CURRENT_SHELL" != "$ZSH_PATH" ]; then
-    read -p "是否将 Zsh 设置为默认 Shell? (y/n): " set_default
+    set_default="y"  # 自动设置为默认
     if [[ "$set_default" =~ ^[Yy]$ ]]; then
         chsh -s "$ZSH_PATH" root
         log "Zsh 已设置为默认 Shell (重新登录后生效)" "info"
