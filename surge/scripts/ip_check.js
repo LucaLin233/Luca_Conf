e36aa3028428673157d5ac5c5f6482d08846f42b
LucaLin <78164141+LucaLin233@users.noreply.github.com>
2026-09-13 03:59:20 +0800

    文件名：多词标识补下划线（模块/规则/脚本），保持全小写

--- surge/scripts/ip_check.js
+++ surge/scripts/ip_check.js
@@ -7,7 +7,7 @@
 * ...
 * [Script]
 * #节点检测
-* ipcheck = type=generic,timeout=3,script-path=https://raw.githubusercontent.com/LucaLin233/Proxy/main/surge/scripts/ipcheck.js
+* ipcheck = type=generic,timeout=3,script-path=https://raw.githubusercontent.com/LucaLin233/Proxy/main/surge/scripts/ip_check.js
 */
 
 let url = "http://ip-api.com/json/?lang=zh-CN"
